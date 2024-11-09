0834d79d9aa2efb91aca0e1750f8e96ad984fd3f
serhiiSavchenko2 <152009512+serhiiSavchenko2@users.noreply.github.com>
2024-11-09 16:43:20 +0100

    Lesson 09/11
    Updated to current state (Start of ExpressJS)

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"/><button type="submit">Send</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => { 
+    res.send('<h1>Hello from Root!</h1>');
+});
+
+app.listen(8100);
