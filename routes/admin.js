fccc6a7356c8ce7cf9eec5e2129653388bf2e759
serhiiSavchenko2 <152009512+serhiiSavchenko2@users.noreply.github.com>
2024-11-23 14:18:14 +0100

    Заняття від 23.11
    Додана допоміжна функція. Початок роботи з каскадними таблицями стилів.

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"/><button type="submit">Send</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
