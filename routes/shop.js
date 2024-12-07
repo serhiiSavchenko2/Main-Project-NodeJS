9bf8d0113b0fbf13a21b8ebd114d56e85bb4585e
serhiiSavchenko2 <152009512+serhiiSavchenko2@users.noreply.github.com>
2024-12-07 09:16:39 +0100

    Додані розшарювані дані
    Додані розшарювані дані для послідуючого використання шаблонізаторів

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 //  E:\views\shop.html
 //  E:/views/shop.html
 router.get('/', (req, res, next) => { 
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
