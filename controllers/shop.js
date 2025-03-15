43cbb69c3d9e88dd1585746c82f93c93e53738fc
serhiiSavchenko2 <152009512+serhiiSavchenko2@users.noreply.github.com>
2025-03-15 11:02:33 +0100

    Заняття 23
    Доданий модуль Замовлень та реалізовано оформлення замовлень

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { where } = require('sequelize');
 
-exports.getProducts = (req, res, next) => { 
+exports.getProducts = (req, res, next) => {
     Product.findAll()
         .then(products => {
             res.render('shop/product-list', {
@@ -35,7 +36,7 @@ exports.getProduct = (req, res, next) => {
     //     .catch(err => console.log(err));
 };
 
-exports.getIndex = (req, res, next) => { 
+exports.getIndex = (req, res, next) => {
     Product.findAll()
         .then(products => {
             res.render('shop/index', {
@@ -77,15 +78,16 @@ exports.postCart = (req, res, next) => {
                 product = products[0];
             }
             if (product) {
-                //...
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product;
             }
-            return Product.findByPk(prodId)
-                .then(product => {
-                    return fetchedCart.addProduct(product, {
-                        through: { quantity: newQuantity }
-                    });
-                })
-                .catch(err => console.log(err));
+            return Product.findByPk(prodId);
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, {
+                through: { quantity: newQuantity }
+            });
         })
         .then(() => {
             res.redirect('/cart');
@@ -95,10 +97,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDelete = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
-        res.redirect('/cart');
-    });
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts({ where: { id: prodId } });
+        })
+        .then(products => {
+            const product = products[0];
+            return product.cartItem.destroy();
+            //return products[0].cartItem.destroy();
+        })
+        .then(result => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
 }
 
 
@@ -110,8 +121,40 @@ exports.getCheckout = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        pageTitle: 'Your Orders',
-        path: '/orders'
-    });
+    req.user.getOrders({include: ['products']})
+        .then(orders => {
+            res.render('shop/orders', {
+                pageTitle: 'Your Orders',
+                path: '/orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+    let productList;
+    let fetchedCart;
+    req.user.getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts();
+        })
+        .then(products => {
+            productList = products;
+            return req.user.createOrder();
+        })
+        .then(order => {
+            return order.addProducts(productList.map(product => {
+                product.orderItem = { quantity: product.cartItem.quantity };
+                return product;
+            }));
+        })
+        .then(result => {
+            return fetchedCart.setProducts(null);
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
 };
