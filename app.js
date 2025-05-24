e1b9fa77ae9dbfe89c47e524ab264eca4309d199
serhiiSavchenko2 <152009512+serhiiSavchenko2@users.noreply.github.com>
2025-05-24 16:51:11 +0200

    Заняття 34
    Додані flash-параметри

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const User = require('./models/user');
 const MONGODB_URI = 'mongodb+srv://serhiisavchenko2:<pass>@cluster0.aw9rm.mongodb.net/shop';
@@ -40,6 +41,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -53,6 +55,13 @@ app.use((req, res, next) => {
         .catch(err => console.log(err));
     //next();
 });
+
+app.use((req, res, next) => {
+    res.locals.isAuth = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
